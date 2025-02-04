aa68780d49a3cf6baf965a46da67c78032c33fc1
SeonHyeonWook <sonhyunuk@gmail.com>
2025-02-04 22:32:53 +0900

    test

--- src/screens/Ranking.js
+++ src/screens/Ranking.js
@@ -17,19 +17,19 @@ const Ranking = () => {
 
   // 예시 데이터
   const friendRanking = [
-    { rank: 1, userName: "user 1", leaf: 8, seed: 0, time: "00 시간 00 분" },
-    { rank: 2, userName: "user 2", leaf: 4, seed: 1, time: "00 시간 00 분" },
-    { rank: 3, userName: "user 3", leaf: 2, seed: 1, time: "00 시간 00 분" },
-    { rank: 4, userName: "user 4", leaf: 1, seed: 1, time: "00 시간 00 분" },
+    { rank: 1, userName: "user 1", leaf: 8, seed: 0, time: "10 시간 20 분" },
+    { rank: 2, userName: "user 2", leaf: 4, seed: 1, time: "09 시간 13 분" },
+    { rank: 3, userName: "user 3", leaf: 2, seed: 1, time: "06 시간 54 분" },
+    { rank: 4, userName: "user 4", leaf: 1, seed: 1, time: "05 시간 33 분" },
   ];
 
   const allRanking = [
-    { rank: 1, userName: "user 1", leaf: 8, seed: 0, time: "00 시간 00 분" },
-    { rank: 2, userName: "user 2", leaf: 4, seed: 1, time: "00 시간 00 분" },
-    { rank: 3, userName: "user 3", leaf: 2, seed: 1, time: "00 시간 00 분" },
-    { rank: 4, userName: "user 4", leaf: 1, seed: 1, time: "00 시간 00 분" },
-    { rank: 5, userName: "user 1", leaf: 1, seed: 0, time: "00 시간 00 분" },
-    { rank: 6, userName: "user 2", leaf: 0, seed: 0, time: "00 시간 00 분" },
+    { rank: 1, userName: "user 1", leaf: 8, seed: 0, time: "10 시간 20 분" },
+    { rank: 2, userName: "user 2", leaf: 4, seed: 1, time: "09 시간 13 분" },
+    { rank: 3, userName: "user 3", leaf: 2, seed: 1, time: "06 시간 54 분" },
+    { rank: 4, userName: "user 4", leaf: 1, seed: 1, time: "05 시간 33 분" },
+    { rank: 5, userName: "user 1", leaf: 1, seed: 0, time: "04 시간 22 분" },
+    { rank: 6, userName: "user 2", leaf: 0, seed: 0, time: "02 시간 44 분" },
   ];
 
   return (
